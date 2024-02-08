7d280c9c4e3de9c275af68da821c72530a8376ef
Luke Stainer <lstainer2002@gmail.com>
2024-02-08 21:10:52 +1030

    started board.py testing

---
+++ tests/board_test.py
@@ -0,0 +1,18 @@
+import unittest
+from .. import board
+
+class BoardTest(unittest.TestCase): 
+    def test_modify_board(self): 
+        pass
+
+    def test_reset_board(self): 
+        pass
+
+    def test_is_occupied(self): 
+        pass
+
+    def test_has_winner(self):
+        pass
+
+if __name__ == "__main__": 
+    unittest.main()
