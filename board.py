9ba83c44eaee8cab37fcba377b14d46399683d3b
Luke Stainer <lstainer2002@gmail.com>
2024-02-08 21:04:31 +1030

    completed has_winner()

--- board.py
+++ board.py
@@ -19,18 +19,36 @@ class Board:
         return "Succesfully modified"
 
     def reset_board(self):
-        for i in range(3): 
-            for j in range(3): 
-                self.grid[i][j] = " "
+        for i in range(9): 
+            self.grid[i] = " "
 
     def is_occupied(self, position) -> bool: 
         if self.grid[position] == " ":
             return False
         return True
     
-    def has_winner() -> (bool, str): 
-        # do dfs to check if has won
-        # [] [] [] 
-        # [] [] [] 
-        # [] [] [] 
-        pass
+    def has_winner(self) -> (str): 
+        #   0  1  2
+        # 0 [] [] []
+        # 1 [] [] []
+        # 2 [] [] []
+
+        # check rows
+        for i in range(0, 9, 3):
+            if self.grid[i] == " ": 
+                continue
+            if self.grid[i] == self.grid[i+1] == self.grid[i+1]: 
+                return self.grid[i]
+
+        # check columns
+        for i in range(3):
+            if self.grid[i] == " ": 
+                continue
+            if self.grid[i] == self.grid[i+3] == self.grid[i+3+3]: 
+                return self.grid[i]
+            
+        # check diagonal 
+        if self.grid[0] == self.grid[4] == self.grid[8]: 
+            return self.grid[0]
+
+        return None
