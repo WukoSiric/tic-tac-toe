75f5db6cfcbbc78c246a6df00069de919b44a846
Luke Stainer <lstainer2002@gmail.com>
2024-02-12 21:39:48 +1030

    has_winner testing

--- tests/board_test.py
+++ tests/board_test.py
@@ -5,7 +5,6 @@ class BoardTest(unittest.TestCase):
 
     def setUp(self): 
         self.board = Board() 
-        pass
     
     def tearDown(self): 
         pass
@@ -33,10 +32,28 @@ class BoardTest(unittest.TestCase):
         self.assertTrue(self.board.grid == empty_grid)
 
     def test_is_occupied(self): 
-        pass
+        self.assertFalse(self.board.is_occupied(3))
+        self.board.modify_board(3, "X")
+        self.assertTrue(self.board.is_occupied(3))
+        self.assertFalse(self.board.is_occupied(0))
+        self.board.modify_board(0, "O")
+        self.assertTrue(self.board.is_occupied(0))
 
-    def test_has_winner(self):
-        pass
+    def test_has_winner_empty(self):
+        self.assertEqual(self.board.has_winner(), None) 
+
+    def test_has_winner_diagonal(self): 
+        self.board.modify_board(0, "X")
+        self.board.modify_board(4, "X")
+        self.board.modify_board(8, "X")
+
+        self.assertEqual(self.board.has_winner(), "X") 
+
+    def test_has_winner_horizontal(self): 
+        self.board.modify_board(0, "X")
+        self.board.modify_board(1, "X")
+        self.board.modify_board(2, "X")
+        self.assertEqual(self.board.has_winner(), "X") 
 
 if __name__ == "__main__": 
     unittest.main()
