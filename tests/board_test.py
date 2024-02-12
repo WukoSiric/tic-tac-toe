66c86f5dedd2f9245cd5f081a10c12c66637549d
Luke Stainer <lstainer2002@gmail.com>
2024-02-12 21:26:56 +1030

    Properly raise exceptions

--- tests/board_test.py
+++ tests/board_test.py
@@ -10,24 +10,27 @@ class BoardTest(unittest.TestCase):
     def tearDown(self): 
         pass
 
-    def test_modify_board(self): 
+    def test_modify_board_exceptions(self): 
         with self.assertRaises(TypeError): 
             self.board.modify_board(1.1, "X")
+
         with self.assertRaises(IndexError): 
             self.board.modify_board(100, "X")
+
         with self.assertRaises(IndexError): 
             self.board.modify_board(-1, "X")
+
         with self.assertRaises(IndexError): 
             self.board.modify_board(-100, "X")
+
         with self.assertRaises(IndexError): 
-            self.board.modify_board(10, "X")
-        with self.assertRaises(TypeError): 
-            self.board.modify_board("Invalid player_id")
+            self.board.modify_board(position=10, player_id="X")
 
     def test_reset_board(self): 
         self.board.modify_board(3, "X")
         self.board.reset_board() 
-        self.assertTrue(self.board.grid == [" ", " ", " "] * 3)
+        empty_grid = [" ", " ", " "] * 3
+        self.assertTrue(self.board.grid == empty_grid)
 
     def test_is_occupied(self): 
         pass
