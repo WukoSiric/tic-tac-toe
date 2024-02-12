7259eb2a0768fdec1f4149937bb320c3d3e23bef
Luke Stainer <lstainer2002@gmail.com>
2024-02-12 22:30:42 +1030

    running game

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,15 +1,31 @@
 from board import Board
 from player import Player
+from enemy import Enemy
 
-board = Board() 
-player = Player()
 
-print("Setting up board!")
+def main(): 
+    board = Board() 
+    player = Player()
+    enemy = Enemy()
+    game_over = False
 
-board.modify_board(2, "X")
-board.modify_board(1, "X")
-board.modify_board(0, "O")
-print(board.grid)
-print(board.has_winner())
-board.reset_board() 
-print(board.grid)
+# Start while loop
+    print("Setting up board!")
+    board.reset_board() 
+    board.display_board()
+
+    while not board.is_full() and board.has_winner() == None:
+        player.make_move(board)
+        board.display_board()
+
+        print("The enemy makes their move!")
+        enemy.make_move(board)
+        board.display_board()
+
+    winner = board.has_winner()
+    if winner == None:
+        print("No one has won... ")
+    print(winner + " wins the round!")
+
+if __name__ == "__main__": 
+    main() 
