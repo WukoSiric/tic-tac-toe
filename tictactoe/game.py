b63f1947ce2972fd2874ad1bebf89b6bf540dcab
Luke Stainer <lstainer2002@gmail.com>
2024-02-13 00:16:33 +1030

    Refactored while loop

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -15,17 +15,21 @@ def main():
     board.reset_board() 
     display_board(board)
 
-    while not board.is_full() and board.has_winner() == None:
+    while not board.is_full() and board.has_winner() is None:
         clear_screen()
         display_board(board)
         player.make_move(board)
-        enemy.make_move(board)
+
+        if not board.is_full():
+            enemy.make_move(board)
+
 
     clear_screen()
     display_board(board)
     winner = board.has_winner()
     if winner == None:
         print("No one has won... ")
+        return
     print(winner + " wins the round!")
 
 if __name__ == "__main__": 
