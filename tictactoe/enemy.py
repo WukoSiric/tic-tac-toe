b316a30bccf7b22901233946f7cff96b91adf64a
Luke Stainer <lstainer2002@gmail.com>
2024-02-13 01:34:16 +1030

    Created terminal, player_turn, and get_value functions

--- tictactoe/enemy.py
+++ tictactoe/enemy.py
@@ -1,5 +1,7 @@
 from board import Board
 import random
+import math 
+from enum import Enum
 
 class Enemy:
     def __init__(self): 
@@ -13,7 +15,29 @@ class Enemy:
                 break
 
     # Minimax functions 
-    # def terminal_state
-    # def player_turn
-    # def get_actions
-    # def get_Value
+    def terminal_state(self, board: Board) -> bool:
+        if board.is_full(): 
+            return True 
+        if board.has_winner() == "X" or board.has_winner() == "O": 
+            return True
+        return False
+    
+    def player_turn(self, board: Board) -> str:
+        x_count = board.grid.count("X")
+        o_count = board.grid.count("O")
+
+        if x_count <= o_count:
+            return "X"
+        else:
+            return "O"
+
+    def get_actions(self, board: Board):
+        pass
+        
+    def get_value(self, board: Board) -> int:
+        if board.has_winner() == "X": 
+            return -1
+        elif board.has_winner() == "O": 
+            return 1 
+        else: 
+            return 0
