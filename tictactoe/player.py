770515645403c0367fafe4ffcd19ddd8f9afc5f0
Luke Stainer <lstainer2002@gmail.com>
2024-02-13 16:14:36 +1030

    Game doesnt end after incorrect inputs

--- tictactoe/player.py
+++ tictactoe/player.py
@@ -9,9 +9,20 @@ class Player:
         print("Make your move (Enter 0 to 9): ")
 
         while True: 
-            position = int(input())
-            if board.is_occupied(position):
-                print("Position is occupied. Try again:")
-                continue 
+            position = input()
+            try: 
+                position = int(position)
+            except ValueError: 
+                print("Not an integer between 0 and 9. Try again: ")
+                continue
+
+            try: 
+                if board.is_occupied(position):
+                    print("Position is occupied. Try again:")
+                    continue 
+            except IndexError: 
+                print("Index is not between 0 and 9. Try again: ")
+                continue
+
             board.modify_board(position, "X")
             return
